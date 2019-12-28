d2a6e173fe6f1c11556211a2af91ef20e4c85604
Nadezhda Hristova <epohster@gmail.com>
2019-12-28 19:21:34 +0200

    Gallery item close redirects to gallery

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,14 +1,15 @@
 const hamburger = document.getElementById("header-hamburger");
-const navigation = document.getElementById("header-navigation");
 
-hamburger.addEventListener("click", (event) => {
+hamburger && hamburger.addEventListener("click", (event) => {
+    const navigation = document.getElementById("header-navigation");
     navigation.classList.toggle("header-navigation--open");
     event.stopPropagation();
 });
 
 const galleryInfoIcon = document.getElementById("info-icon");
-const galleryInfoBlock = document.getElementById("gallery-item-info")
 
 galleryInfoIcon && galleryInfoIcon.addEventListener("click", (event) => {
-    galleryInfoBlock.classList.toggle("gallery-item--visible")
-})
+    const galleryInfoBlock = document.getElementById("gallery-item-info");
+
+    galleryInfoBlock.classList.toggle("gallery-item--visible");
+});
